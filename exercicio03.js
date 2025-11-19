c1bc1bb9d5572c6fa64edf0e5c2d11369fdf1c87
HEVILYN CHRISTINE FARIAS FERREIRA <62219388301@imaccommand035.local>
2025-11-19 14:55:26 -0300

    Finalizando os exercicios

--- exercicio03.js
+++ exercicio03.js
@@ -1,4 +1,7 @@
-let impar = prompt ("Digite seu número de usuario")
-if (impar) {
-    
-}
+let numero =  parseInt(prompt ("Digite seu número de usuario"))
+
+if (numero % 2 === 0) {
+    console.log("O número" + numero + "é  par")   
+}else{
+    console.log("O número" + numero + "é impar")
+ }
